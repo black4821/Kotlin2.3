3b97a9df8d4d5af582688ff004e457ed184fec7c
black4821 <56718434+black4821@users.noreply.github.com>
2020-07-18 21:48:27 +0700

    save

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.box_two_img)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
@@ -28,14 +28,14 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
 
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
         val clickableViews: List<View> =
-            listOf(boxOneText, boxTwoText, boxThreeText,
+            listOf(boxOneText, boxThreeText,
                 boxFourText, boxFiveText, rootConstraintLayout)
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
